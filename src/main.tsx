4ef28884d06f124022fb55892df2a7bd437b2e3e
OlenaHlu <glushenkova_es@ukr.net>
2024-11-22 01:52:46 +0200

    main

---
+++ src/main.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { BrowserRouter } from "react-router-dom";
+import { store, persistor } from "./redux/store";
+import App from "./App.tsx";
+import "./style.css";
+
+const rootElement = document.getElementById("root") as HTMLElement;
+
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </PersistGate>
+      </Provider>
+    </React.StrictMode>
+  );
+}
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <PersistGate persistor={persistor}>
+//         <BrowserRouter>
+//           <App />
+//         </BrowserRouter>
+//       </PersistGate>
+//     </Provider>
+//   </React.StrictMode>
+// );
