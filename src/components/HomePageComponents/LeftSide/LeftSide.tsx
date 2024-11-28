3c6d59883e14a26ac598ee94711a4039ad86ab65
OlenaHlu <glushenkova_es@ukr.net>
2024-11-28 02:55:43 +0200

    finish left container

--- src/components/HomePageComponents/LeftSide/LeftSide.tsx
+++ src/components/HomePageComponents/LeftSide/LeftSide.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import Icon from "../../common/Icon";
 import css from "./LeftSide.module.css";
 
 const LeftSide = () => {
@@ -8,7 +9,8 @@ const LeftSide = () => {
         <h1 className={css.title}>Make Life Easier for the Family:</h1>
         <p className={css.text}>Find Babysitters Online for All Occasions</p>
         <Link to="/nannies" className={css.link}>
-          Get started{" "}
+          Get started
+          <Icon className={css.icon} iconName="arrow-up"></Icon>
         </Link>
       </div>
     </div>
